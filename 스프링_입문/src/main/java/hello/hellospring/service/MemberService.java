88fa89d79c3b985fbc03ab9a35976772244f132f
YounYoung Heo <112863029+Heo-y-y@users.noreply.github.com>
2023-09-06 15:55:54 +0900

    Add files via upload

---
+++ 스프링_입문/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,38 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(user -> {
+                    throw new IllegalStateException("이미 존재하는 회원이다.");
+                });
+    }
+}
